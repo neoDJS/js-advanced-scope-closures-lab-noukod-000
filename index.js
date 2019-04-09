2aae4187b3dc35db045e02a68a356e3be6015753
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 16:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function produceDrivingRange(range){
-  return function(){
-
+  return function(first, second){
+        const distance = parseInt(second) - parseInt(first);
+        if(distance > range){
+            return `${distance - blockRange} blocks out of range`;
+        }else{
+            return `within range by ${distance}`;
+        }
   }
 }
