426b20a8ab404db80fc88e8cba64e9eee0d807a2
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 16:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function createDriver(){
     let driverId = 0;
 
     return class Driver{
-        constructor(name){
-            this.name = name;
+        constructor(){
+            // this.name = name;
             this.id = ++driverId;
         }
     }
